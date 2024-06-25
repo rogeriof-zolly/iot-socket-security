4d764ef379360ccf4ce77e7bcf9501ec52fca04d
rogeriof-zolly <rogeriozolly603@gmail.com>
2024-06-25 00:46:21 -0300

    certificate encryption and decryption

--- AES/AWS/servidor.py
+++ AES/AWS/servidor.py
@@ -1,16 +1,17 @@
 # echo-server.py
 import time
 import socket
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
 from Crypto.Util.Padding import unpad
 from sha256 import calculate_hash
 from mockKMS import MockKMS
+import base64
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65431  # Port to listen on (non-privileged ports are > 1023)
 
 kms_client = MockKMS()
-
 
 
 while True:
@@ -26,8 +27,13 @@ while True:
                   break
               start_time = time.perf_counter_ns()
               obj = AES.new(kms_client.get_encryption_key().encode(), AES.MODE_CBC, kms_client.get_initialization_vector())
+              key = RSA.import_key(open('mycert.key').read())
+              cipher = PKCS1_OAEP.new(key)
 
-              text = unpad(obj.decrypt(data), 16)
+              aes_decrypted = unpad(obj.decrypt(data), 16)
+              base64_bytes = base64.b64decode(aes_decrypted)
+              text = cipher.decrypt(base64_bytes)
+
               hash_received = text[-64:].decode()
               message_hash = calculate_hash(text[:-64].decode())
               if hash_received != message_hash:
--- AES/raspberryPi/cliente.py
+++ AES/raspberryPi/cliente.py
@@ -3,10 +3,13 @@ import time
 import socket
 from faker import Faker
 
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, PKCS1_OAEP
 from sha256 import calculate_hash
 from Crypto.Util.Padding import pad
+from Crypto.PublicKey import RSA
 from mockKMS import MockKMS
+import base64
+
 start_time = time.perf_counter_ns()
 
 HOST = "127.0.0.1"
@@ -16,7 +19,10 @@ kms_client = MockKMS()
 
 obj = AES.new(kms_client.get_encryption_key().encode(), AES.MODE_CBC, kms_client.get_initialization_vector())
 
+key = RSA.import_key(open('mycert.pem').read())
+
 faker = Faker("pt_BR")
+cipher = PKCS1_OAEP.new(key=key)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
@@ -25,7 +31,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
     message = (plate + hash_calculation).encode()
 
-    text = pad(message, 16)
+    post_rsa_message = cipher.encrypt(message)
+    base64_bytes = base64.b64encode(post_rsa_message)
+
+    text = pad(base64_bytes, 16)
     ciphertext = obj.encrypt(text)
 
     s.sendall(ciphertext)
