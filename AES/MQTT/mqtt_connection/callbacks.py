b093ae347be5d1e5740afc127472811ca48efd90
rogeriof-zolly <rogeriozolly603@gmail.com>
2024-07-01 02:52:21 -0300

    remove commented code from callbacks

--- AES/MQTT/mqtt_connection/callbacks.py
+++ AES/MQTT/mqtt_connection/callbacks.py
@@ -1,28 +1,5 @@
 from config.broker_configs import mqtt_broker_configs
-import base64
 from decryption.decryption import decryptMessage
-
-# def decryptMessage(message):
-#   kms_client = MockKMS()
-
-#   obj = AES.new(kms_client.get_encryption_key().encode(),
-#                 AES.MODE_CBC, kms_client.get_initialization_vector())
-#   key = RSA.import_key(open('mycert.key').read())
-#   cipher = PKCS1_OAEP.new(key)
-
-#   aes_decrypted = unpad(obj.decrypt(message), 16)
-#   base64_bytes = base64.b64decode(aes_decrypted)
-#   text = cipher.decrypt(base64_bytes)
-
-#   hash_received = text[-64:].decode()
-#   message_hash = calculate_hash(text[:-64].decode())
-
-#   if hash_received != message_hash:
-#     print("Hashes are different")
-#     return False
-
-#   print(text[:-64])
-#   return True
 
 
 def on_connect(client, userdata, flags, rc):
